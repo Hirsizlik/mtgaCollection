0df5bf96d0ebd1a41a7b22c2fceab0faf011b55c
Markus Schagerl <Markus.Schagerl1@gmx.at>
2021-08-20 13:04:41 +0200

    ProgramArguments: add javadoc and non-null checks

--- src/hirsizlik.mtgacollection/hirsizlik/mtgacollection/arguments/ProgramArguments.java
+++ src/hirsizlik.mtgacollection/hirsizlik/mtgacollection/arguments/ProgramArguments.java
@@ -1,11 +1,25 @@
 package hirsizlik.mtgacollection.arguments;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Holds the given program arguments.
+ *
+ * @author Markus Schagerl
  */
-public record ProgramArguments(String pathToProperties, String pathToDatabase, String runName,
+public record ProgramArguments(
+		/** Given path to the properties file, may be null */
+		String pathToProperties,
+		/** Given path to the database file, may be null */
+		String pathToDatabase,
+		/** Given name of the run to be executed, must not be null */
+		String runName,
+		/** Any additional arguments given, may be empty but not null */
 		List<String> additionalArguments) {
 
+	public ProgramArguments {
+		Objects.requireNonNull(runName);
+		Objects.requireNonNull(additionalArguments);
+	}
 }
