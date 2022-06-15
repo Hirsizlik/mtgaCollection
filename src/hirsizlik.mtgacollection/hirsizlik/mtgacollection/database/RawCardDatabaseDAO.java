d31069eaba4138040532a25acccff80b84097033
Markus Schagerl <Markus.Schagerl1@gmx.at>
2022-06-15 13:20:06 +0200

    Add a DAO to access the RawCardDatabase of MTGA

---
+++ src/hirsizlik.mtgacollection/hirsizlik/mtgacollection/database/RawCardDatabaseDAO.java
@@ -0,0 +1,88 @@
+package hirsizlik.mtgacollection.database;
+
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Access to Raw_CardDatabase in MTGA to load localized names of cards.
+ *
+ * @author Markus Schagerl
+ */
+public class RawCardDatabaseDAO implements AutoCloseable {
+
+	private final Connection c;
+	private PreparedStatement readLocalizationStatement;
+
+	/**
+	 * Constructs a new instance and opening a read-only connection to the database
+	 * @param toDB the path to the database
+	 * @throws SQLException thrown by DriverManager.getConnection (probably database not found)
+	 * and Connection#prepareStatement (some SQL error, should never happen).
+	 */
+	public RawCardDatabaseDAO(final Path toDB) throws SQLException {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("org.sqlite.JDBC could not be loaded", e);
+		}
+		Properties p = new Properties();
+		p.setProperty("open_mode", "1");// "1" == read only
+		c = DriverManager.getConnection("jdbc:sqlite:" + toDB.toAbsolutePath().toString(), p);
+		c.setReadOnly(true);
+	}
+
+	/**
+	 * Returns the English localization for the given id.
+	 * @param localizationId the id
+	 * @return the English, non-formatted value
+	 * @throws SQLException SQL error, for example if no localization with that id is found
+	 */
+	public String getEnglishNonFormattedLocalization(final int localizationId) throws SQLException {
+		if (readLocalizationStatement == null) {
+			readLocalizationStatement = c.prepareStatement(
+					"SELECT l.Formatted, l.enUs from Localizations l where l.LocId = ?");
+		}
+
+		readLocalizationStatement.setLong(1, localizationId);
+		readLocalizationStatement.execute();
+
+		ResultSet rs = readLocalizationStatement.getResultSet();
+		record LocResult(boolean formatted, String enUs) {}
+
+		List<LocResult> results = new ArrayList<>();
+		while (rs.next()) {
+			boolean formatted = rs.getInt(1) == 1;
+			String enUs = rs.getString(2);
+			results.add(new LocResult(formatted, enUs));
+		}
+
+		// there should be either no, one or two results
+		// zero -> no result found, throw a SQLException
+		// one -> a result with formatted = 1 (but doesn't use any formatting)
+		// two -> one result with HTML-Tags, one without, use the one without
+		if (results.isEmpty()) {
+			throw new SQLException("No Localization with LocId %d found".formatted(localizationId));
+		} else if (results.size() == 1) {
+			return results.get(0).enUs;
+		} else {
+			return results.stream()
+					.filter(lr -> !lr.formatted)
+					.map(lr -> lr.enUs)
+					.findFirst()
+					.orElseThrow(() -> new IllegalStateException("Multiple Localizations for "
+							+ "%d found, but none of them are not formatted".formatted(localizationId)));
+		}
+	}
+
+	@Override
+	public void close() throws SQLException {
+		c.close();
+	}
+}
