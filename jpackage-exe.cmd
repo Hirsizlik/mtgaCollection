ad7114c011eb39c63b993608e8afe34b445cd63b
Markus Schagerl <Markus.Schagerl1@gmx.at>
2022-06-17 14:32:02 +0200

    update java to 18, add release for deb + exe, change create-release.yml
    and update moditect-maven-plugin to 1.0.0.RC2

---
+++ jpackage-exe.cmd
@@ -0,0 +1,11 @@
+jpackage ^
+-n mtgacollection ^
+--runtime-image .\target\jlink-image ^
+--module hirsizlik.mtgacollection/hirsizlik.mtgacollection.main.MTGACollectionMain ^
+-t exe ^
+--description "A CLI program to see stats of your MTG-Arena card collection." ^
+--app-version %1 ^
+-d .\target ^
+--license-file .\LICENSE ^
+--win-console ^
+--verbose 
