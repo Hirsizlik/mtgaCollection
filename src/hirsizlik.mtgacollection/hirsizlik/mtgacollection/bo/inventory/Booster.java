4946e993bf1e5fab962b006d91efbfcde3be546d
Markus Schagerl <Markus.Schagerl1@gmx.at>
2022-06-19 12:34:36 +0200

    Booster: add Set field, remove getPackCodeFromCollation
    now the set code is in the inventory, no need to parse the collationId
    like before (still needed for mythic packs, but that's an easy check)

--- src/hirsizlik.mtgacollection/hirsizlik/mtgacollection/bo/inventory/Booster.java
+++ src/hirsizlik.mtgacollection/hirsizlik/mtgacollection/bo/inventory/Booster.java
@@ -2,57 +2,51 @@ package hirsizlik.mtgacollection.bo.inventory;
 
 /**
  * Describes the amount of booster of a set currently owned.
- *
+ * <p>
+ * Note about collationId:
+ * There a 6 groups of collationIds:
+ * <ol>
+ * <li>&lt; 100000: "KLD" and "AER" from the Arena beta
+ * <li>100000 - 199999: Premier packs from the shop
+ * <li>200000 - 299999: Draft packs (used during draft)
+ * <li>300000 - 399999: Cube packs
+ * <li>400000 - 499999: Alchemy packs
+ * <li>500000 - 599999: Mythic packs
+ * </ol>
  * @author Markus Schagerl
  */
 public record Booster(
 		/** the amount of booster packs currently owned*/
 		int count,
-		/** the collationId, can be mapped to set code */
-		Integer collationId) {
+		/** the collationId, used to determine if a mythic pack or not */
+		Integer collationId,
+		/** the set code (MID, Y22NEO, etc.)*/
+		String set) {
 
 	/**
 	 * @return a short string describing this booster.
-	 * <div>
+	 * <p>
 	 * Format is {@code <set code>: <amount of booster>}"
-	 * </div>
 	 */
 	public String getShortString() {
-		return "%s: %d".formatted(getSetCodeFromCollation(), count);
+		if (isMythicPack()) {
+			return "%s (Mythic): %d".formatted(set, count);
+		}
+		// set code of Alchemy packs is e.g. Y22NEO, no extra check needed
+		return "%s: %d".formatted(set, count);
 	}
 
-	// can't be imported from the game as it is hard coded there, see "CollationMapping"-class
-	// Note: collationId of draft packs start with 2, cubes start wit 3. both not included here
 	/**
-	 * Returns the set code corresponding to this packs collationId.
-	 *
-	 * @return the set code of this booster, or it's collationId if unknown.
+	 * @return true if this pack is an alchemy pack
 	 */
-	public String getSetCodeFromCollation() {
-		return switch(collationId) {
-		case 62242 -> "KLD";
-		case 62979 -> "AER";
-		case 100003 -> "AKH";
-		case 100004 -> "HOU";
-		case 100005 -> "XLN";
-		case 100006 -> "RIX";
-		case 100007 -> "DAR";
-		case 100008 -> "M19";
-		case 100009 -> "GRN";
-		case 100010 -> "RNA";
-		case 100013 -> "WAR";
-		case 100014 -> "M20";
-		case 100015 -> "ELD";
-		case 100016 -> "THB";
-		case 100017 -> "IKO";
-		case 100018 -> "M21";
-		case 100019 -> "AKR";
-		case 100020 -> "ZNR";
-		case 100021 -> "KLR";
-		case 100022 -> "KHM";
-		case 100023 -> "STX";
-		case 100024 -> "AFR";
-		default -> collationId.toString();
-		};
+	public boolean isAlchemyPack() {
+		return collationId >= 400000 && collationId < 500000;
+	}
+
+	/**
+	 * @return true if this pack is a mythic pack
+	 */
+	public boolean isMythicPack() {
+		return collationId >= 500000 && collationId < 600000;
 	}
 }
