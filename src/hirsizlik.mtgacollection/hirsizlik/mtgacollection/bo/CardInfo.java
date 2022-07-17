0fcbb5d8e51b0937954891543b81057e8eb4b855
Markus Schagerl <Markus.Schagerl1@gmx.at>
2022-07-17 12:34:37 +0200

    remove default and UNKNOWN values

--- src/hirsizlik.mtgacollection/hirsizlik/mtgacollection/bo/CardInfo.java
+++ src/hirsizlik.mtgacollection/hirsizlik/mtgacollection/bo/CardInfo.java
@@ -1,22 +1,8 @@
 package hirsizlik.mtgacollection.bo;
 
-import hirsizlik.mtgacollection.database.SetInfoLoader;
-
 /**
  * Information about a card from a specific set.
  *
  * @author Markus Schagerl
  */
-public record CardInfo(int id, String name, Rarity rarity, SetInfo set, boolean inBooster) {
-
-	/**
-	 * Creates a "unknown" card. Used as a placeholder if the real card cannot be found.
-	 *
-	 * @param id the id of the unknown card
-	 * @param setInfoLoader the setInfoLoader to load the unknown set
-	 * @return the new unknown card with the given id
-	 */
-	public static CardInfo newUnknown(final int id, final SetInfoLoader setInfoLoader) {
-		return new CardInfo(id, "Unknown", Rarity.UNKNOWN, setInfoLoader.getUnknownSet(), true);
-	}
-}
+public record CardInfo(int id, String name, Rarity rarity, SetInfo set, boolean inBooster) {}
